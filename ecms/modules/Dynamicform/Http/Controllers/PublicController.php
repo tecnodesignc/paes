c45d6722ee939f6b92acbf50fafbe1a9fcf7ee17
Marcos <marcos21.009@gmail.com>
2024-02-01 16:28:14 -0500

    updated dashboard

--- ecms/modules/Dynamicform/Http/Controllers/PublicController.php
+++ ecms/modules/Dynamicform/Http/Controllers/PublicController.php
@@ -2,19 +2,22 @@
 
 namespace modules\Dynamicform\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Modules\Core\Http\Controllers\Admin\AdminBaseController;
-use Modules\Dynamicform\Http\Requests\CreateFormRequest;
-use Modules\Dynamicform\Repositories\FormRepository;
+use Modules\Dynamicform\Repositories\FormResponseRepository;
+use Modules\Dynamicform\Transformers\FormResponseTransformer;
 
 class PublicController extends AdminBaseController
 {
+    private FormResponseRepository $form_response;
 
-    public function __construct()
+    public function __construct(FormResponseRepository $form_response)
     {
         parent::__construct();
+        $this->form_response = $form_response;
 
     }
 
@@ -26,7 +29,24 @@ class PublicController extends AdminBaseController
     public function dashboard():Application|Factory|View
     {
 
-        return view('modules.dynamic-form.index');
+        $params = json_decode(json_encode([
+            'filter' => [
+                'date' => [
+                    'field' => 'start_date',
+                    'from' => Carbon::now()->setTime(0, 0, 0)->format('Y-m-d H:i:s'),
+                    'to' => Carbon::now()->format('Y-m-d H:i:s')
+                ],
+                'companies' => company()->id?company()->id:array_values(companies()->where('type',1)->map(function ($company){
+                    return $company->id;
+                })->toArray())
+            ], 'include' => 'form', 'page' => 1, 'take' => 10000
+        ]));
+
+        $forms_response=$this->form_response->getItemsBy($params);
+
+        $forms_response=FormResponseTransformer::collection($forms_response);
+
+        return view('modules.dynamic-form.index',compact('forms_response'));
     }
 
 }
