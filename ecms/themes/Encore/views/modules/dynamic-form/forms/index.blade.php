a78f48e8c722550ea233898de3328a088144c748
Davilson <53828575+Davilson26@users.noreply.github.com>
2024-03-15 11:16:51 -0500

    Update index.blade.php

--- ecms/themes/Encore/views/modules/dynamic-form/forms/index.blade.php
+++ ecms/themes/Encore/views/modules/dynamic-form/forms/index.blade.php
@@ -143,10 +143,11 @@
                         formatter: (cell, row) => {
                             let actionsHtml = '<div class="d-flex justify-content-center align-items-center gap-4">';
                             actionsHtml += '<a href="/preoperativo/form/'+ row.cells[0].data + '/show" data-bs-toggle="tooltip" data-bs-placement="top" title="Vista previa" class="text-info"><i class="mdi mdi-eye-outline me-1 mdi-24px"></i></a>';
-                            if ({{$currentUser->hasAccess('dynamicform.forms.edit')}}) {
+                            let hasAccessEdit = {{$currentUser->hasAccess('dynamicform.forms.edit') ? 'true' : 'false'}};
+                            if (hasAccessEdit) {
                                 actionsHtml +=
-                                  '<a href="/preoperativo/form/' + row.cells[0].data + '/edit" data-bs-toggle="tooltip" data-bs-placement="top" title="Editar" class="text-success btn-lg"><i class="mdi mdi-clipboard-edit-outline mdi-24px"></i></a>'
-                                + '<a href="" data-bs-toggle="tooltip" data-bs-placement="top" title="Borrar" onclick="deleteField(event, '+ row.cells[0].data +')" >' + (row.cells[4].data == '1' ? '<i class="mdi mdi-lock-open mdi-24px"></i>' : '<i class="mdi mdi-lock mdi-24px text-secondary"></i>') + '</a>';
+                                    '<a href="/preoperativo/form/' + row.cells[0].data + '/edit" data-bs-toggle="tooltip" data-bs-placement="top" title="Editar" class="text-success btn-lg"><i class="mdi mdi-clipboard-edit-outline mdi-24px"></i></a>'
+                                    + '<a href="" data-bs-toggle="tooltip" data-bs-placement="top" title="Borrar" onclick="deleteField(event, '+ row.cells[0].data +')" >' + (row.cells[4].data == '1' ? '<i class="mdi mdi-lock-open mdi-24px"></i>' : '<i class="mdi mdi-lock mdi-24px text-secondary"></i>') + '</a>';
                             }
 
                             actionsHtml += '</div>';
